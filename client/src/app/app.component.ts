b449c7db7fa2ac6ba9273b37d13b99a7c1541aa6
ADM3M <fly.swift499@gmail.com>
2022-05-20 23:11:17 +0300

    Initialized client

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  
+  title = 'client';
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  
+}
