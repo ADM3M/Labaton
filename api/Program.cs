40cd31627ffaca9e520cbcd19dae49c69fa2ecff
ADM3M <fly.swift499@gmail.com>
2022-05-24 01:17:17 +0300

    Configured CORS

--- api/Program.cs
+++ api/Program.cs
@@ -1,13 +1,24 @@
+using api.Data;
+using api.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IFolderRepository, FolderRepository>();
 
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseCors(policy => policy
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .AllowAnyHeader()
+                .WithOrigins("http://localhost:4200"));
 
 app.UseAuthorization();
 
