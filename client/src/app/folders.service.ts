601d2a4707cffa208a3ab718c38071cb6462d085
ADM3M <fly.swift499@gmail.com>
2022-05-24 20:45:14 +0300

    Implemented folder service.

--- client/src/app/folders.service.ts
+++ client/src/app/folders.service.ts
@@ -1,12 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { IFolderCreate } from 'src/Models/IFolderCreate';
+import { INode } from 'src/Models/INode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoldersService {
 
+  public data: INode;
+  public current: INode;
+  
   constructor(private http:HttpClient) { }
 
-  // getFolders()
+  private baseUrl = environment.baseUrl;
+  
+  getFolders() {
+    return this.http.get<INode>(this.baseUrl + "folders");
+  }
+
+  createFolder(folderCreateDTO: IFolderCreate) {
+    return this.http.post<INode>(this.baseUrl + "folders/create", folderCreateDTO);
+  }
 }
